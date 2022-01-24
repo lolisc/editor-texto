0b87468992dc14b15f8f8a3b5eb9d09bb16fb652
Lorena Neves <lorenanevescarneiroc@gmail.com>
2022-01-24 19:21:35 -0300

    Implement download button.

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+var textFile = null
+
+function makeTextFile(text) {
+    var data = new Blob([text], { type: 'text/plain' });
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+        window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    return textFile;
+};
+
+var test = makeTextFile('ronaldo')
+console.log(test)
+
 function botaoclick() {
     const doc = document.getElementById("texto").value
-    localStorage.setItem("text", doc)
+
+    var link = document.getElementById('downloadlink');
+    link.href = makeTextFile(doc);
+    // link.style.display = 'block';
+    link.click();
+
 }
+
 document.getElementById("botao").addEventListener("click", botaoclick)
-document.getElementById("texto").value = localStorage.getItem("text")
